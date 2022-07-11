ed8b353717c8eb476f99f415bcdfafa54bec7ee1
Morgan Hamet <morganhamet@gmail.com>
2022-07-10 18:56:12 -0700

    Redo hero image

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,20 +1,28 @@
 import Nav from "./Nav";
 import "../styles/style.css";
 import { Link } from "react-router-dom";
-
+import iceland from "../styles/images/iceland.jpg";
 
 function Home(props) {
   return (
     <div className="container">
       <Nav amount={props.amount}/>
       <main className="home-container">
-          <div className="home-text">
-            <h1>The Hangul Collection</h1>
-            <p>Check out the smart styles of the Korean language.</p>
-            <Link className="home-link" style={{textDecoration: "none"}} to="/hangul-shop">
-              <button className="home-button">SHOP NOW</button>
-            </Link>
+          <div className="hero-container">
+            <div className="hero"></div>
+            <div className="home-text">
+              <h1>OUT NOW: HANGUL</h1>
+              <p>Check out the smart styles of the Korean language.</p>
+              <Link className="home-link" style={{textDecoration: "none"}} to="/hangul-shop">
+                <button className="home-button">SHOP NOW</button>
+              </Link>
+            </div>
           </div>
+          {/* <div className="iceland-container">
+            <img src={iceland} alt="" />
+            <h2>The Land of Ice and Fire</h2>
+            <p>Shop the cool, classic look of Icelandic.</p>
+          </div> */}
       </main>
     </div>
   );
